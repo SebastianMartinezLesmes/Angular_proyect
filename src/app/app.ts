a5d6ce901c9cb5ff97efd13fa4c195a393ae77c8
Lesmes <20rasputin03@gmail.com>
2025-09-09 17:58:42 -0500

    CREATE: page for apply a line time

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
 })
 export class App {
   protected readonly title = signal('angular');
--- src/app/pages/time-line/time-line.ts
+++ src/app/pages/time-line/time-line.ts
@@ -1,25 +1,48 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router'; 
+import { HttpClient } from '@angular/common/http';
+import Swal from 'sweetalert2';
+import { TimelineModule } from 'primeng/timeline'; // 👈 IMPORTANTE
+import { CardModule } from 'primeng/card'; // opcional para tarjetas
+import { TagModule } from 'primeng/tag';   // opcional para tags
 
 @Component({
   selector: 'app-time-line',
   templateUrl: './time-line.html',
-  styleUrl: './time-line.scss',
-  imports: [],
+  styleUrls: ['./time-line.scss'],
+  standalone: true,
+  imports: [ TimelineModule, CardModule, TagModule], // 👈 aquí
 })
 export class TimeLineComponent {
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+  ) {}
 
-  constructor(private router: Router) {}
+  eventos: any[] = [];
 
   listOfThings = [
     { label: 'Home', path: '/home' },
     { label: 'Logout', path: '/login' },
   ];
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getData();
+  };
 
   routerTo(path: string) {
     this.router.navigate([path]);
-  }
+  };
 
+  getData() {
+    this.http.get<any[]>('/json/historico.json').subscribe({
+      next: (data) => {
+        this.eventos = data;
+        console.log("✅ Datos cargados:", this.eventos);
+      },
+      error: (err) => {
+        console.error("❌ Error cargando JSON:", err);
+      }
+    })
+  };
 }
